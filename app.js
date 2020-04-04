761676c06c6c0943105a776ce07e0970652ccfe3
Filippo Massarelli <massa@filippos-mbp.lan>
2020-04-04 16:37:23 +0100

    Change encryption to hashing passwords

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -26,7 +27,7 @@ const userSchema = new mongoose.Schema({
 });
 
 //define secret for mongoose-encryption and add encreption to userSchema
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -47,7 +48,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password) //Hash the password
   });
   newUser.save(function(err){
     if (err){
@@ -60,7 +61,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password); //Hash input string from user to compare with hashed pswd
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
